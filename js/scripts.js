33fcf8bece94666212b8fa7cd3fc346d120e3a2c
jared <jaredspecjr@gmail.com>
2018-03-30 09:43:54 -0700

    add functions for 0 to say beep and and other number to say boop

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+debugger;
+function numerals0(input) {
+  if (input === 0){
+    return "beep";
+  }
+}
+function numerals(number) {
+  var output = "";
+  for (var i = 1; i <= number; i++){
+    output = "boop";
+  }
+  return output;
+}
