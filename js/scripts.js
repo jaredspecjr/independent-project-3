dc6121de2400930b41e28d673160de3824aee75b
jared <jaredspecjr@gmail.com>
2018-03-30 11:24:35 -0700

    add more logic to main function, still not completely working

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-debugger;
 function zeros(number) {
   if (number === 0){
     return "beep";
@@ -16,12 +15,17 @@ function dave(number) {
   }
   return output;
 }
-
+//stuck on what to do here.
 var beepboop = function(userInput){
+  debugger;
   var result = [];
   for (var i = 0; i <= userInput; i++){
     if (i === 0){
       result.push("beep");
+    } else if (i === 1){
+      result.push("boop")
+    } else if ( i % 3 === 0){
+      result.push("I'm sorry, dave. I'm afraid I can't do that.")
     }
   }
   return result;
